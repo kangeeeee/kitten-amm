6345dd34022b9412aecb4a7ae117a6bd15c14644
kitten-master <dgdev@naver.com>
2021-05-14 22:55:18 +0900

    init

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x019be1796178516e060072004f267b59a49a0801',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xE3B4eEDb56260bbF0Da83733fecd23140FF188b9'
+}
+
+export default priceContracts
